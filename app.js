270be3ad67f7f07c7873dbc895239309f078ac32
lisamarie616 <lisamarie616@gmail.com>
2016-08-08 10:52:17 -0700

    add bootstrap styling to campgrounds page

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set('view engine', 'ejs');
 var campgrounds = [
   {name: 'Salmon Creek', image: 'https://pixabay.com/static/uploads/photo/2016/06/06/08/32/tent-1439061__180.jpg'},
   {name: 'Granite Hill', image: 'https://pixabay.com/static/uploads/photo/2013/09/16/19/15/camp-182951__180.jpg'},
+  {name: "Mountain Goat's Rest", image: 'https://pixabay.com/static/uploads/photo/2015/02/26/15/41/ginger-cat-650545__180.jpg'},
+  {name: 'Salmon Creek', image: 'https://pixabay.com/static/uploads/photo/2016/06/06/08/32/tent-1439061__180.jpg'},
+  {name: 'Granite Hill', image: 'https://pixabay.com/static/uploads/photo/2013/09/16/19/15/camp-182951__180.jpg'},
+  {name: "Mountain Goat's Rest", image: 'https://pixabay.com/static/uploads/photo/2015/02/26/15/41/ginger-cat-650545__180.jpg'},
+  {name: 'Salmon Creek', image: 'https://pixabay.com/static/uploads/photo/2016/06/06/08/32/tent-1439061__180.jpg'},
+  {name: 'Granite Hill', image: 'https://pixabay.com/static/uploads/photo/2013/09/16/19/15/camp-182951__180.jpg'},
   {name: "Mountain Goat's Rest", image: 'https://pixabay.com/static/uploads/photo/2015/02/26/15/41/ginger-cat-650545__180.jpg'}
 ]
 
