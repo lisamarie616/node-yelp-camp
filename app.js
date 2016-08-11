2252b2db8047a893043af7810c70016f8b64f654
lisamarie616 <lisamarie616@gmail.com>
2016-08-11 14:25:07 -0700

    add styles to campgrounds show page

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ seedDB();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.render('landing');
 });
@@ -50,7 +52,6 @@ app.get('/campgrounds/:id', function(req, res){
     if(err){
       console.log(err);
     } else {
-      console.log(foundCampground);
       res.render('campgrounds/show', {campground: foundCampground});
     }
   });
